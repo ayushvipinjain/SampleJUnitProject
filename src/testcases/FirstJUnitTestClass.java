4bb4a149f31fade76731dae15cf5076db627254d
Ayush Jain <jayush@thoughtworks.com>
2018-05-07 11:48:38 +0530

    Initial Commit for JUnit Project

---
+++ src/testcases/FirstJUnitTestClass.java
@@ -0,0 +1,35 @@
+package testcases;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class LearningJUnit {
+
+
+    @Test
+    @Ignore
+    public void First()
+    {
+        PrintMessage("Inside First Test");
+    }
+
+    @Test
+    public void Second()
+    {
+        PrintMessage("Inside Second Test");
+    }
+
+    @Test
+    public void Third()
+    {
+        PrintMessage("Inside Third Test");
+    }
+
+    private void PrintMessage(String message)
+    {
+        System.out.println("Initiate Printing Messages");
+        System.out.println(message);
+        System.out.println("End Printing Messages");
+
+    }
+}
